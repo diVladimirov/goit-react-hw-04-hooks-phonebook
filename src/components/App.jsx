65a5511a613cb1f4423484f9bcf1ae5b65b9dd07
Vadim Vladimirov <kurtonchik@gmail.com>
2022-01-23 11:28:24 +0200

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,35 +7,3 @@ export const App = () => {
     </div>
   );
 };
-
-// class App extends React.Component {
-//   state = {
-//     contacts: [],
-//     name: '',
-//   };
-
-//   formSubmitHandler = data => {
-//     console.log(data);
-//     // const todo = {
-//     //   id: nanoid(),
-//     //   data,
-//     // };
-
-//     // this.setState(prevState => ({
-//     //   todos: [todo, ...prevState.todos],
-//     // }));
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Phonebook</h1>
-//         <Form onSubmit={this.formSubmitHandler} />
-//         <h2>Contacts</h2>
-//         <p></p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,8 +4,8 @@ import * as Yup from 'yup';
 import { nanoid } from 'nanoid';
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().min(2, 'Too Short!').max(10, 'Too Long!').required(),
-  number: Yup.number().required(),
+  name: Yup.string().required().min(2, 'Too Short!').max(20, 'Too Long!'),
+  number: Yup.number().required().min(2),
 });
 
 const initialValues = {
@@ -19,23 +19,19 @@ const FormError = ({ name }) => {
 };
 
 export class FormView extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
   handleSubmit = ({ name, number }, { resetForm }) => {
-    // const isNameInContacts = this.props.contacts.find(
-    //   contact => contact.name.toLowerCase() === name.toLowerCase(),
-    // );
-
-    // if (isNameInContacts) {
-    //   alert(`${name} is already in contacts`);
-    //   return;
-    // }
-
     const contactObj = { id: nanoid(4), name, number };
-    console.log(contactObj);
-    this.props.onSubmit(contactObj);
+    this.setState({ contacts: [contactObj, ...this.state.contacts] });
     resetForm();
   };
 
   render() {
+    const { contacts } = this.state;
     return (
       <div>
         <h1>Phonebook</h1>
@@ -59,52 +55,15 @@ export class FormView extends Component {
           </Form>
         </Formik>
         <h2>Contacts</h2>
+        <ul>
+          {contacts.map(el => (
+            <li key={el.id}>
+              <span>{el.name}</span>
+              <span>{el.number}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
-
-// class Form extends Component {
-//   state = {
-//     name: '',
-//   };
-
-//   handleInputChange = event => {
-//     // console.log(event.currentTarget);
-//     // console.log(event.currentTarget.name);
-//     // console.log(event.currentTarget.value);
-//     const { name, value } = event.currentTarget;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state.name);
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({ name: '' });
-//   };
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           <input
-//             type="text"
-//             name="name"
-//             value={this.state.name}
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//             onChange={this.handleInputChange}
-//           />
-//         </label>
-//         <button type="submit">Add contact</button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default Form;

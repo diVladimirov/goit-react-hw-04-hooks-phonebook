bd57fa9724f5f06e56b1f34d019f2ba472ef7064
Vadim Vladimirov <kurtonchik@gmail.com>
2022-01-24 15:24:05 +0200

    add saving to LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,14 +10,23 @@ import GlobalStyle from '../constants/GlobalStyle';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   onFormSubmit = ({ name, number }) => {
     const nameToAdd = this.state.contacts.find(
@@ -28,7 +37,7 @@ class App extends Component {
     }
 
     this.setState(({ contacts }) => ({
-      contacts: [{ id: nanoid(), name, number }, ...contacts],
+      contacts: [{ id: nanoid(4), name, number }, ...contacts],
     }));
   };
 
